6f8663c2b1bef5f0503d33b35047e3e67a5c6dda
Agustin Romero <88514423+agustin-romero@users.noreply.github.com>
2021-11-01 18:57:15 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+let nombre = "Agustin";
+const styles = {
+  parrafo: {
+    fontFamily: "roboto",
+    color: "red"
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ color: "blue" }}>Hola Mundo!</h1>
+      <p className="parrafo2">Ecommerce en proceso</p>
+      <p style={styles.parrafo}>Nombres: {nombre}</p>
+      <p style={styles.parrafo}>Edad: {16+6}</p>
+    </>
   );
 }
 
